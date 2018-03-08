25ac97c2b7ea8da6dd1a5bfbc5090ed3bc882a67
Sam Gould <sjg326@gmail.com>
2018-03-08 13:36:27 -0800

    updated bucket sort to handle duplicates

--- bucketSort.js
+++ bucketSort.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*
 Bucket sort
 Write an O(n) algorithm to sort an array of integers, where you know in advance what the lowest and highest values are.
@@ -19,7 +21,11 @@ const bSort = (array, lowestValue, highestValue) => {
   }
   let tempArray = [];
   for (let i=0; i < array.length; i++){
-      tempArray[array[i] - lowestValue] = 1 //1 truthy; 0 falsy
+    if (!tempArray[array[i] - lowestValue]) {
+      tempArray[array[i] - lowestValue] = 1;
+    } else {
+      tempArray[array[i] - lowestValue] = tempArray[array[i] - lowestValue] + 1;
+    }
   }
   let i = 0;
 
@@ -27,12 +33,17 @@ const bSort = (array, lowestValue, highestValue) => {
     if(tempArray[j] === 1){
       array[i] = j + lowestValue;
       i++;
+    } else if (tempArray[j] > 1) {
+      for (let k=0; k < tempArray[j]; k++) {
+        array[i] = j + lowestValue;
+        i++;
+      }
     }
   }
 
   return array;
-}
+};
 
 //console.log(bSort([89, 30, 25, -32, 72],-32,89));
-console.log(bSort([89, 30, 25, 32, 72],25,89));
-
+// console.log(bSort([89, 30, 25, 32, 72],25,89));
+console.log(bSort([89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5], 1, 98));

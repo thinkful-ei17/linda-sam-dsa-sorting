d5e79fd974cc4ad0f178b7e23d5217dc05ab8db5
Linda Foo <linda.foo.dev@gmail.com>
2018-03-08 11:37:41 -0800

    added count for quick and merge

--- mergeSort.js
+++ mergeSort.js
@@ -1,7 +1,9 @@
 'use strict';
 
+let count = 0;
 const mSort = array => {
   if (array.length <= 1) {
+    count++;
     return array;
   }
 
@@ -21,6 +23,7 @@ const merge = (left, right, array) => {
   let outputIndex = 0;
 
   while (leftIndex < left.length && rightIndex < right.length) {
+    count++;
     if (left[leftIndex] < right[rightIndex]) {
       // iteration happens after the value declaration -- could be done in two parts of declaration and then iterate after in lines 27 and 28
       array[outputIndex++] = left[leftIndex++];
@@ -32,15 +35,18 @@ const merge = (left, right, array) => {
   }
 	
   for (let i=leftIndex; i < left.length; i++) {
+    count++;
     array[outputIndex++] = left[i];
   }
 	
   for (let i=rightIndex; i < right.length; i++) {
+    count++;
     array[outputIndex++] = right[i];
 	}
 	
 	return array;
 };
 
-// console.log(mSort([89, 30, 25, 32, 72]));
-console.log(mSort([89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5]));
+console.log(mSort([89, 30, 25, 32, 72]));
+//console.log(mSort([89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5]));
+console.log(count);
